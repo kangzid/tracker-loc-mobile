ab55d8676e50e68e206802d3e4f326acdde17ab2
kangzid <zidanalmubarok652@gmail.com>
2016-02-18 12:00:00 +0700

    push one

--- lib/pages/employee/screens/track_employee.dart
+++ lib/pages/employee/screens/track_employee.dart
@@ -1,12 +1,219 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/auth/auth_storage.dart';
 
-class TrackEmployeePage extends StatelessWidget {
+class TrackEmployeePage extends StatefulWidget {
   const TrackEmployeePage({super.key});
 
   @override
+  State<TrackEmployeePage> createState() => _TrackEmployeePageState();
+}
+
+class _TrackEmployeePageState extends State<TrackEmployeePage> {
+  bool _autoTrackingEnabled = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Track Employee Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Tracker Saya",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: FutureBuilder<Map<String, dynamic>?>(
+        future: AuthStorage().getLoginData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData && snapshot.data != null) {
+            final loginData = snapshot.data!;
+            final user = loginData['user'];
+            final name = user['name'] ?? 'N/A';
+            final employeeId = user['employee']['employee_id'] ?? 'N/A';
+            final position = user['employee']['position'] ?? 'N/A';
+
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Bagian Atas: Card berisi informasi singkat employee
+                  Card(
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            '$employeeId • $position',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text(
+                            'Status Absensi Hari Ini:',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const Text(
+                            'Belum Check-in', // Dummy text
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+
+                  // Bagian Tengah: Placeholder map
+                  Container(
+                    height: 250,
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Map Placeholder",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+
+                  // Bagian Bawah: Kontrol tracking
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // TODO: Implement update location logic
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Update Lokasi Sekarang clicked!')),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: const Text(
+                          "Update Lokasi Sekarang",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Auto Tracking",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Switch(
+                            value: _autoTrackingEnabled,
+                            onChanged: (bool value) {
+                              setState(() {
+                                _autoTrackingEnabled = value;
+                              });
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Auto Tracking: ${value ? "On" : "Off"}')),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Card(
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Ringkasan Dashboard Mini",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              _buildDashboardRow("Jumlah tugas aktif:", "0"),
+                              _buildDashboardRow("Lokasi terakhir update:", "-"),
+                              _buildDashboardRow("Absensi status:", "-"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const Center(child: Text('No login data found.'));
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildDashboardRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(fontSize: 14, color: Colors.black87),
+          ),
+          Text(
+            value,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+        ],
+      ),
     );
   }
 }
